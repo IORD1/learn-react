8a5aa956e8a3d02e6d19c41be7a524b77b479660
IORD1 <pratham111ingole@gmail.com>
2022-07-31 00:03:12 +0530

    updated some

--- quoter/src/App.js
+++ quoter/src/App.js
@@ -5,6 +5,12 @@ import quotonquot from './qotonqot.svg';
 import './App.css';
 
 class App extends Component {
+
+  getdata(){
+    alert("yo");
+    // $getJSON("https://www.colr.org/json/colors/random/1", function(data) {})
+  }
+
   render() {
     return (
       <div className="App">
@@ -27,7 +33,6 @@ class App extends Component {
           <div className='main-box'>
             <div className='quote-box'>
               <div className='quote-container'>
-                {/* <img src={quotonquot} className="quotonquot" alt="quotes" /> */}
                 <i class="fa fa-quote-left">
                   
                 </i>
@@ -44,18 +49,19 @@ class App extends Component {
                   <i class="fa fa-twitter"></i>
                 </a> 
                 
-                <a href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Farrah%20Gray&content=Build%20your%20own%20dreams%2C%20or%20someone%20else%20will%20hire%20you%20to%20build%20theirs.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button" className="footer-button">
+                <a href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Farrah%20Gray&content=Build%20your%20own%20dreams%2C%20or%20someone%20else%20will%20hire%20you%20to%20build%20theirs.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button" className="footer-button" id='tumblr-button'>
                   <i class="fa fa-tumblr"></i>
                 </a> 
                 
-                <div id='newq' className="footer-button">
-
+                <div id='newq' className="footer-button-holder">
+                  <button className='footer-button' id='foot-button' onClick={this.getdata}>New quote</button>
                 </div>
 
               </div>
             </div>
             <div className='by-box'>
-
+                <p class="by-footer">by </p>
+                <p className='by-pratham'>pratham-_-</p>
             </div>
 
           </div>
