210fd08f48801368f95a2cb4340d7f289fe29c29
IORD1 <pratham111ingole@gmail.com>
2022-08-04 19:06:04 +0530

    project quoter completed

--- quoter/src/App.js
+++ quoter/src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
     super(props)
     this.state = {
       bgcolor: 'blue',
-      text : "pratham ingole",
-      test : ''
+      text : "Loading",
+      auth : 'click below button'
     }
   }
 
@@ -22,11 +22,6 @@ class App extends Component {
   func = () => {
     theme= '#'+Math.floor(Math.random()*16777215).toString(16);
     this.setState({bgcolor: theme});
-    this.setState({text : 'app is great'});
-
-    // fetch("https://api.quotable.io/random")
-    //               .then((res) => res.json())
-    //               .then((json) => {console.log(alert('"' + json.author + '"'))})
     
     
     const that = this;
@@ -35,14 +30,9 @@ class App extends Component {
           return response.json();
       })
       .then(function(jsonData) {
-          return JSON.stringify(jsonData);
-      })
-      .then(function(jsonStr) {
-          that.setState({ 
-            text: jsonStr.valueOf("author")
-            
-          });
-          console.log(jsonStr);
+          that.setState({auth : jsonData.author});
+          that.setState({text : jsonData.content});
+          //return JSON.stringify(jsonData.content);
       });
 
           
@@ -75,28 +65,28 @@ class App extends Component {
           <div className='main-box'>
             <div className='quote-box'>
               <div className='quote-container'>
-                <i class="fa fa-quote-left">
+                <i class="fa fa-quote-left"  style={{color: this.state.bgcolor}} >
                   
                 </i>
-                <p className='quote' id='quotep'>
+                <p className='quote' id='quotep'  style={{color: this.state.bgcolor}} >
                     {this.state.text}
                 </p>
               </div>
               <div className='by-container'>
                 <p>-</p>
-                <p className='by'>Farrah Gray</p>
+                <p className='by'  style={{color: this.state.bgcolor}} >{this.state.auth}</p>
               </div>
               <div className='quote-share-container'>
-                <a href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp" target="_blank" className="footer-button">
+                <a href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp" target="_blank" className="footer-button" style={{backgroundColor: this.state.bgcolor}} >
                   <i class="fa fa-twitter"></i>
                 </a> 
                 
-                <a href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Farrah%20Gray&content=Build%20your%20own%20dreams%2C%20or%20someone%20else%20will%20hire%20you%20to%20build%20theirs.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button" className="footer-button" id='tumblr-button'>
+                <a href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Farrah%20Gray&content=Build%20your%20own%20dreams%2C%20or%20someone%20else%20will%20hire%20you%20to%20build%20theirs.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button" className="footer-button" id='tumblr-button' style={{backgroundColor: this.state.bgcolor}} >
                   <i class="fa fa-tumblr"></i>
                 </a> 
                 
                 <div id='newq' className="footer-button-holder">
-                  <button className='footer-button' id='foot-button' onClick={this.func}>New quote</button>
+                  <button className='footer-button' id='foot-button' onClick={this.func} style={{backgroundColor: this.state.bgcolor}} >New quote</button>
                 </div>
 
               </div>
