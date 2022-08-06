eed79a24c8ca6e4015f8f601ab1bb7dd0bb23f0d
IORD1 <pratham111ingole@gmail.com>
2022-08-06 20:21:36 +0530

    able to convert string to markdown

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+//import $ from 'jquery';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      initial: ' # Welcome to my React Markdown Previewer! '
 
-  func = () => {
-    
-
-          
+    }
   }
 
+ 
+
+
+
+  func = () => {
+    this.setState({initial : document.getElementById('markdown').value}); 
+    document.getElementById('content').innerHTML = window.marked.parse(this.state.initial);  
+        
+  }
+
+  componentDidMount(){
+    this.func();
+  } 
+
+
+
   render() {
+
+
+
     return (
+      
       <div className="App">
+
         <div id='mardkdown-cont'>
           <div id='markdown-box'>
             <div id='markdown-head'>
@@ -21,7 +43,7 @@ class App extends Component {
             <button id="markdown-but"><i class="fa fa-expand"></i></button>
             </div>
             <div id='markdown-rest'>
-              <textarea type="type" onChange={this.func}></textarea>
+              <textarea id="markdown" onChange={this.func} >{this.state.initial}</textarea>
             </div>
 
           </div>
@@ -31,10 +53,14 @@ class App extends Component {
         <div id='preview-cont'>
           <div id='preview-box'>
             <div id='preview-head'>
-            (<i class="fa fa-fire"></i>)
-            <p>Previewer</p> 
-            <button id="markdown-but"><i class="fa fa-expand"></i></button>
+              (<i class="fa fa-fire"></i>)
+              <p>Previewer</p> 
+              <button id="preview-but"><i class="fa fa-expand"></i></button>
             </div>
+            <div id='preview-rest'>
+              <div id="content"></div>
+            </div>
+
             
             </div>
         </div>
@@ -44,3 +70,14 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+{/* 
+<script>
+  document.getElementById('content').innerHTML =
+    marked.parse('# Marked in the browser\n\nRendered by **marked**.');
+</script> */
+
+// document.getElementById('marked').innerText
+}
