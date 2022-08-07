a57962403c5eb03f8ac4e3363214f3890cc26963
IORD1 <pratham111ingole@gmail.com>
2022-08-08 01:16:30 +0530

    finished second project

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -3,55 +3,72 @@ import './App.css';
 //import $ from 'jquery';
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
-      initial: ' # Welcome to my React Markdown Previewer! '
-
+      initial: ' # Welcome to my React Markdown Previewer! ',
+      max: 1,
+      max1: 1,
+      heightc: 200,
+      heightp: 300
     }
   }
 
- 
-
-
 
   func = () => {
-    this.setState({initial : document.getElementById('markdown').value}); 
-    document.getElementById('content').innerHTML = window.marked.parse(this.state.initial);  
-        
+    this.setState({ initial: document.getElementById('markdown').value });
+    document.getElementById('content').innerHTML = window.marked.parse(this.state.initial);
+
   }
 
   func2 = () => {
-    document.getElementById('content').innerHTML = window.marked.parse(document.getElementById('markdown').value); 
+    document.getElementById('content').innerHTML = window.marked.parse(document.getElementById('markdown').value);
   }
 
-  componentDidMount(){
+  func3 = () => {
+    this.setState({ max: 1 });
+    if (this.state.max == 1) {
+      this.setState({ heightc: "80vh" });
+      this.setState({ max: 2 });
+    } else {
+      this.setState({ heightc: "200px" });
+      this.setState({ max: 1 });
+    }
+
+  }
+
+  func4 = () => {
+    this.setState({ max1: 1 });
+    if (this.state.max1 == 1) {
+      this.setState({ heightp: "80vh" });
+      this.setState({ max1: 2 });
+    } else {
+      this.setState({ heightp: "300px" });
+      this.setState({ max1: 1 });
+    }
+
+  }
+
+  componentDidMount() {
     this.func2();
-  } 
-
-
+  }
 
   render() {
 
-
-
     return (
-      
+
       <div className="App">
 
         <div id='mardkdown-cont'>
           <div id='markdown-box'>
             <div id='markdown-head'>
-            (<i class="fa fa-fire"></i>)
-            <p>Editor</p>  
-            <button id="markdown-but"><i class="fa fa-expand"></i></button>
+              (<i class="fa fa-fire"></i>)
+              <p>Editor</p>
+              <button id="markdown-but" onClick={this.func3}><i class="fa fa-expand"></i></button>
             </div>
             <div id='markdown-rest'>
-              <textarea id="markdown" onChange={this.func2} >
-                
-              
-                 
-                 </textarea>
+              <textarea id="markdown" placeholder='Try Something like "# your-name" to preview it below' onChange={this.func2} style={{ minHeight: this.state.heightc }} >
+              </textarea>
             </div>
 
           </div>
@@ -62,15 +79,13 @@ class App extends Component {
           <div id='preview-box'>
             <div id='preview-head'>
               (<i class="fa fa-fire"></i>)
-              <p>Previewer</p> 
-              <button id="preview-but"><i class="fa fa-expand"></i></button>
+              <p>Previewer</p>
+              <button id="preview-but" onClick={this.func4}><i class="fa fa-expand"></i></button>
             </div>
             <div id='preview-rest'>
-              <div id="content"></div>
+              <div id="content" style={{ minHeight: this.state.heightp }}>Your Preview will apear here</div>
             </div>
-
-            
-            </div>
+          </div>
         </div>
       </div>
     );
@@ -78,14 +93,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-{/* 
-<script>
-  document.getElementById('content').innerHTML =
-    marked.parse('# Marked in the browser\n\nRendered by **marked**.');
-</script> */
-
-// document.getElementById('marked').innerText
-}
--- quoter/src/App.js
+++ quoter/src/App.js
@@ -6,41 +6,33 @@ import './App.css';
 
 var theme = '#FA6864';
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       bgcolor: 'blue',
-      text : "Loading",
-      auth : 'click below button'
+      text: "Loading",
+      auth: 'click below button'
     }
   }
 
-
-
   func = () => {
-    theme= '#'+Math.floor(Math.random()*16777215).toString(16);
-    this.setState({bgcolor: theme});
-    
-    
+    theme = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    this.setState({ bgcolor: theme });
+
+
     const that = this;
     fetch("https://api.quotable.io/random")
-      .then(function(response) {
-          return response.json();
+      .then(function (response) {
+        return response.json();
       })
-      .then(function(jsonData) {
-          that.setState({auth : jsonData.author});
-          that.setState({text : jsonData.content});
-          //return JSON.stringify(jsonData.content);
+      .then(function (jsonData) {
+        that.setState({ auth: jsonData.author });
+        that.setState({ text: jsonData.content });
+        //return JSON.stringify(jsonData.content);
       });
-
-          
   }
 
-
   render() {
-
-    
-
     return (
 
       <div className="App">
@@ -54,44 +46,44 @@ class App extends Component {
           <div className="logoheader2">
             <img src={sharingan} className="App-logo" alt="logo" />
           </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
         </div>
 
-        <div className='app-base' style={{backgroundColor: this.state.bgcolor}} >
+        <div className='app-base' style={{ backgroundColor: this.state.bgcolor }} >
           <div className='main-box'>
             <div className='quote-box'>
               <div className='quote-container'>
-                <i class="fa fa-quote-left"  style={{color: this.state.bgcolor}} >
-                  
+                <i class="fa fa-quote-left" style={{ color: this.state.bgcolor }} >
+
                 </i>
-                <p className='quote' id='quotep'  style={{color: this.state.bgcolor}} >
-                    {this.state.text}
+                <p className='quote' id='quotep' style={{ color: this.state.bgcolor }} >
+                  {this.state.text}
                 </p>
               </div>
               <div className='by-container'>
                 <p>-</p>
-                <p className='by'  style={{color: this.state.bgcolor}} >{this.state.auth}</p>
+                <p className='by' style={{ color: this.state.bgcolor }} >{this.state.auth}</p>
               </div>
               <div className='quote-share-container'>
-                <a href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp" target="_blank" className="footer-button" style={{backgroundColor: this.state.bgcolor}} >
+                <a href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp" target="_blank" className="footer-button" style={{ backgroundColor: this.state.bgcolor }} >
                   <i class="fa fa-twitter"></i>
-                </a> 
-                
-                <a href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Farrah%20Gray&content=Build%20your%20own%20dreams%2C%20or%20someone%20else%20will%20hire%20you%20to%20build%20theirs.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button" className="footer-button" id='tumblr-button' style={{backgroundColor: this.state.bgcolor}} >
+                </a>
+
+                <a href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Farrah%20Gray&content=Build%20your%20own%20dreams%2C%20or%20someone%20else%20will%20hire%20you%20to%20build%20theirs.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button" className="footer-button" id='tumblr-button' style={{ backgroundColor: this.state.bgcolor }} >
                   <i class="fa fa-tumblr"></i>
-                </a> 
-                
+                </a>
+
                 <div id='newq' className="footer-button-holder">
-                  <button className='footer-button' id='foot-button' onClick={this.func} style={{backgroundColor: this.state.bgcolor}} >New quote</button>
+                  <button className='footer-button' id='foot-button' onClick={this.func} style={{ backgroundColor: this.state.bgcolor }} >New quote</button>
                 </div>
 
               </div>
             </div>
             <div className='by-box'>
-                <p class="by-footer">by </p>
-                <p className='by-pratham'>pratham-_-</p>
+              <p class="by-footer">by </p>
+              <p className='by-pratham'>pratham-_-</p>
             </div>
 
           </div>
