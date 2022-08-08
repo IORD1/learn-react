e4f0a21e7acf2292e40885aa01e22d5c6a526915
IORD1 <pratham111ingole@gmail.com>
2022-08-08 18:49:47 +0530

    started third project

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -3,16 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  
+
+  func = () => {
+    let aud1 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3");
+    aud1.play();
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div id='box'>
+          <div id='keys'>
+            <div id='q' className='buttons row1' onClick={this.func}>
+              Q
+              </div>
+            <div id='w' className='buttons row1'>W</div>
+            <div id='e' className='buttons row1'>E</div>
+
+            <div id='a' className='buttons row2'>A</div>
+            <div id='s' className='buttons row2'>S</div>
+            <div id='d' className='buttons row2'>D</div>
+
+            <div id='z' className='buttons row3'>Z</div>
+            <div id='x' className='buttons row3'>X</div>
+            <div id='c' className='buttons row3'>C</div>
+
+          </div>
+          <div id='panel'>
+
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
