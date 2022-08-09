2f45ba0a59ea10aac317e6c094ea1ea843a3d1e0
IORD1 <pratham111ingole@gmail.com>
2022-08-09 20:29:03 +0530

    working

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -7,64 +7,99 @@ class App extends Component {
     super(props)
     this.state = {
       power: "1px auto auto 1px",
-      ispower : 1
+      ispower : 1,
+      switch : "1px auto auto 1px",
+      isswitch : 1,
+      display : "",
+      bgc : ""
 
     }
   }
 
 
   func = (key) => {
-    let aud = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-" + key + ".mp3");
-    let aud4 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3");
-    let aud5 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3");
-    let aud6 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3");
-    let aud7 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3");
-    let aud8 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3");
-    let aud9 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3");
-    aud.play();
+  
+    if(this.state.ispower == 1){
+      this.setState({display : "Power : Off"});
+    }else{
 
+      let aud = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-" + key + ".mp3");
+      let aud4 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3");
+      let aud5 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3");
+      let aud6 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3");
+      let aud7 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3");
+      let aud8 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3");
+      let aud9 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3");
+      aud.play();
+      if(key == 1){
+        this.setState({display : "Heater 1"});
 
+        aud.play();
+      }else if(key == 2){
+        this.setState({display : "Heater 2"});
+        aud.play();
+      }else if(key == 3){
+        this.setState({display : "Heater 3"});
+        aud.play();
+      }else if(key == 4){
+        this.setState({display : "Heater 4"});
+        aud4.play();
+      }else if(key == 5){
+        this.setState({display : "Clap"});
+        aud5.play();
+      }else if(key == 6){
+        this.setState({display : "Open HH"});
+        aud6.play();
+      }else if(key == 7){
+        this.setState({display : "Kick n' Hat"});
+        aud7.play();
+      }else if(key == 8){
+        this.setState({display : "Kick"});
+        aud8.play();
+      }else if(key == 9){
+        this.setState({display : "Closed HH"});  
+        aud9.play();
+      }
 
-    if(key == 1){
-      aud.play();
-    }else if(key == 2){
-      aud.play();
-    }else if(key == 3){
-      aud.play();
-    }else if(key == 4){
-      aud4.play();
-    }else if(key == 5){
-      aud5.play();
-    }else if(key == 6){
-      aud6.play();
-    }else if(key == 7){
-      aud7.play();
-    }else if(key == 8){
-      aud8.play();
-    }else if(key == 9){
-      aud9.play();
-    }
+   }
   }
 
 
   power = () => {
     if(this.state.ispower == 1){
+      this.setState({display : "Power : On"});
       this.setState({ power: "1px 1px auto auto"});
       this.setState({ispower : 0});
     }else{
+      this.setState({display : "Power : Off"});
       this.setState({power : "1px auto auto 1px"});
       this.setState({ispower : 1});
     }
   }
 
+  switch = () => {
+    if(this.state.isswitch == 1){
+      this.setState({display : "Smooth Paino Kit"});
+      this.setState({ switch: "1px 1px auto auto"});
+      this.setState({isswitch : 0});
+    }else{
+      this.setState({display : "Heater Kit"});
+      this.setState({switch : "1px auto auto 1px"});
+      this.setState({isswitch : 1});
+    }
+  }
+
+  setvol = () => {
+    this.setState({display : "Vol : " + document.getElementById('slider').value * 10})
+  }
+
+  
   render() {
     return (
       <div className="App">
         <div id='box'>
           <div id='keys'>
-            <div id='q' className='buttons row1' onClick={() => this.func(1)}>
-              Q
-              </div>
+            <div id='q' className='buttons row1' onClick={() => this.func(1)}>Q</div>
             <div id='w' className='buttons row1' onClick={() => this.func(2)}>W</div>
             <div id='e' className='buttons row1' onClick={() => this.func(3)}>E</div>
 
@@ -92,13 +127,23 @@ class App extends Component {
                 </div>
               </div>
               <div id='display-cont' className='panel-mem'>
+                <p id='display'>{this.state.display}</p>
 
               </div>
               <div id='slider-cont' className='panel-mem'>
-
+                <input  id='slider' type="range" min="0" max="1" step="0.1" onChange={this.setvol}></input>
               </div>
               <div id='switch-cont' className='panel-mem'>
+              <div id='switch-head'>
+                  <p>Bank</p>
+                </div>
+                <div id='switch-box'>
+                  <div id='switch'>
+                    <div id='switch-slider' onClick={this.switch} style={{ margin : this.state.switch }}>
 
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
 
