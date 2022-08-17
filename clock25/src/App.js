103f6d9aaa1c89e2ed9100923371a274430be3a7
IORD1 <pratham111ingole@gmail.com>
2022-08-18 00:13:44 +0530

    front end for fifth project done

--- clock25/src/App.js
+++ clock25/src/App.js
@@ -13,29 +13,39 @@ class App extends Component {
           <div id='time-edit'>
             <div id='left-edit'>
               <div id='left-edit-up' className='edit-up-down'>Break Length</div>
-              <div id='left-edit-down' className='edit-up-down'>2</div>
+              <div id='left-edit-down' className='edit-up-down'>
+                <div id='left-edit-inc' className='arrows'><span class="material-symbols-outlined">arrow_downward</span></div>
+                <div id='left-edit-num' className='arrows'>5</div>
+                <div id='left-edit-dec'className='arrows'><span class="material-symbols-outlined">arrow_upward</span></div>
+                </div>
             </div>
             <div id='right-edit'>
               <div id='right-edit-up' className='edit-up-down'>Session Length</div>
-              <div id='right-edit-down' className='edit-up-down'>4</div>
+              <div id='right-edit-down' className='edit-up-down'>
+                <div id='right-edit-inc' className='arrows'><span class="material-symbols-outlined">arrow_downward</span></div>
+                <div id='right-edit-num' className='arrows'>5</div>
+                <div id='right-edit-dec'className='arrows'><span class="material-symbols-outlined">arrow_upward</span></div>
+              </div>
             </div>
           </div>
           <div id='main'>
             <div id='main-box'>
               <div id='main-head'>
-
+                Session
               </div>
               <div id='main-time'>
-                
+                25:00
               </div>
             </div>
 
           </div>
           <div id='button-box'>
-
+            <div id='play' className='buttons'><span class="material-symbols-outlined">play_arrow</span></div>
+            <div id='pause' className='buttons'><span class="material-symbols-outlined">pause</span></div>
+            <div id='restart' className='buttons'><span class="material-symbols-outlined">restart_alt</span></div>
           </div>
           <div id='by-box'>
-
+          Designed and Coded by Pratham-_-
           </div>
         </div>
       </div>
